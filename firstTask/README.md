1f70677f83d99dd7037d5e9f07899eb2427105c6
dani <dani@dani.dani>
2025-07-11 20:30:45 +0300

    first commit

---
+++ firstTask/README.md
@@ -0,0 +1,37 @@
+# Todo List App
+
+A simple todo list web application built using HTML, CSS, and JavaScript.
+
+## Features
+
+- Add tasks
+- Edit tasks
+- Delete tasks
+
+## Demo Screenshot
+
+### Adding Task
+
+![Screenshot](screenshots/add-1.png)
+![Screenshot](screenshots/add-2.png)
+![Screenshot](screenshots/add-3.png)
+
+### Editing a Task
+
+![Screenshot](screenshots/edit-1.png)
+![Screenshot](screenshots/edit-2.png)
+![Screenshot](screenshots/edit-3.png)
+
+### Deleting a Task
+
+![Screenshot](screenshots/edit-3.png)
+![Screenshot](screenshots/delete-1.png)
+
+## How to Run
+
+1. Clone the repository or download the ZIP.
+2. Open `index.html` in a browser.
+
+## Author
+
+Daniel Molla
---
+++ firstTask/main.js
@@ -0,0 +1,49 @@
+const arr = ["learn js"];
+const  cond  = [0,0]
+
+function addToList(){
+    let val = document.getElementById("val")
+    let newvalue = val.value.trim()
+    
+    if( cond[0] == 1){
+        arr[cond[1]] = newvalue;
+        cond[0] = 0
+        let btn = document.getElementById("btn1")
+        btn.innerHTML = "Add"
+        let val = document.getElementById("val")
+        val.value = ""
+
+
+    }
+    else{
+        
+        if (newvalue != "")
+        {
+            arr.push(newvalue)
+            val.value = ""
+        }
+    }
+    showarr()
+    console.log(arr)
+}
+
+function deleteTask(ind){
+ arr.splice(ind,1)
+ showarr()
+}
+function editTask(ind){
+    cond[0] = 1
+    cond[1] = ind
+    let val = document.getElementById("val")
+    let btn = document.getElementById("btn1")
+    val.value = arr[ind];
+    btn.innerHTML = "Edit"
+    showarr()
+}
+function showarr(){
+    let show = document.getElementById("showtask")
+    show.innerHTML = `<h1> let me show u your tasks</h1> 
+    ${arr.map((val,ind)=>`<div class = "row" >  ${ind+1}.${val} <div class ="editdelete"> <button class=${"edit"}keyy= ${ind} onclick=editTask(${ind})> Edit </button> <button key= ${ind} class=${"delete"} onclick=deleteTask(${ind})> delete</button></div></div>`).join("")}`
+}
+showarr()
+
